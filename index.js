ddf80c269ceddfe19afe9bab378c5128f74b6efc
Rajanjor <81916900+123452123-codr@users.noreply.github.com>
2021-08-13 10:51:13 +0530

    resolving some errors due to which the page was not working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var signUp = document.getElementById('signUp');
 var alreadyauser = document.getElementById('alreadyauser');
 
 signUp.addEventListener("click", function(){
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
     .then((userCredential) => {
         var user = userCredential.user;
         const text = {RealEmail : email.value, RealPassword : password.value};
