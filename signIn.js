a80ced62b3fb9620c0b0559a48439385400cf862
Rajanjor <81916900+123452123-codr@users.noreply.github.com>
2021-08-13 15:08:43 +0530

    adding sign in authentication and correcting glitches...

--- signIn.js
+++ signIn.js
@@ -21,4 +21,29 @@ signIn.addEventListener("click", function(){
 
 signInGoogle.addEventListener("click", function(){
     var provider = new firebase.auth.GoogleAuthProvider();
-})
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth()
+    .getRedirectResult()
+    .then((result) => {
+        if (result.credential) {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            window.open("quiz.html","_self");
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        alert(errorMessage);
+        console.log(errorCode);
+    });
+});
